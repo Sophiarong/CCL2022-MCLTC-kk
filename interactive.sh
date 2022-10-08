ccce7362f2e0c571fe7757e807df50aa6a3b26c7
Sophiaroon <1071022927@qq.com>
2022-10-08 11:59:26 +0800

    rename

--- interactive.sh
+++ interactive.sh
@@ -4,8 +4,6 @@ export CUDA_VISIBLE_DEVICES=1
 DATA_SET=data/processed
 MODEL_PATH=/.../bart-baseline
 
-#cat ${DATA_PATH} \
-#    |
 python interactive.py $DATA_SET \
     --user-dir bart-zh \
     --task translation_hf_bart \
@@ -14,4 +12,3 @@ python interactive.py $DATA_SET \
     --left-pad-source \
     --buffer-size 64 \
     --batch-size 64
-#    2>&1 | tee $MODEL_PATH/${SPLIT}.yaclc-${STYLE}.log
